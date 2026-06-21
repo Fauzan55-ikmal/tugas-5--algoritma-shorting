ccf2aaeab190d3f4a028768f244a66b2055b6eb9
Fauzan Fauzia Fauzi <fauzanfauziafauzi@gmail.com>
2026-06-21 22:04:19 +0700

    feat: update sorting header declarations based on lecture code

---
+++ include/sorting.h
@@ -0,0 +1,8 @@
+#ifndef SORTING_H
+#define SORTING_H
+
+void tampilkan_larik(int data[], int n);
+int partisi(int data[], int awal, int akhir);
+void quick_sort(int data[], int awal, int akhir);
+
+#endif
